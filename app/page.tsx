7615fbe44997389617742475b0438c53e6f02f1e
Ivan Martins <ivanmartins.ilha@gmail.com>
2025-07-16 12:42:25 -0300

    feat: adjusted structure for files.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "./_components/HeroSection";
 import ServicesOverview from "./_components/ServicesOverview";
 import HowItWorks from "./_components/HowItWorks";
 import Testimonials from "./_components/Testimonials";
+import VideoSection from "./_components/VideoSection";
 import FinalCTA from "./_components/FinalCTA";
 import Footer from "./_components/Footer";
 import FloatingWhatsApp from "./_components/FloatingWhatsApp";
@@ -14,6 +15,7 @@ export default function Home() {
       <HeroSection />
       <ServicesOverview />
       <HowItWorks />
+      <VideoSection />
       <Testimonials />
       <FinalCTA />
       <Footer />
