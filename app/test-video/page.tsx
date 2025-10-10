3bebe095d2e14d9410560b4490017b62e851c39c
Ivan Martins <ivanmartins.ilha@gmail.com>
2025-10-10 19:03:53 -0300

    feat: creted page video test.

---
+++ app/test-video/page.tsx
@@ -0,0 +1,11 @@
+import VideoSectionTest from "../_components/landing/VideoSectionTest";
+
+export default function TestVideoPage() {
+  return (
+    <div className="min-h-screen">
+      <VideoSectionTest />
+    </div>
+  );
+}
+
+
